9eac57cd24367688e0fa1887d3414a1cfeb124ef
Sergey Batalov <sthror@gmail.com>
2023-08-01 16:04:58 +0300

    fixes error from resolve conflicts

--- src/services/playerCodeService/Queries/GetChannelsByProjectID.ts
+++ src/services/playerCodeService/Queries/GetChannelsByProjectID.ts
@@ -34,8 +34,6 @@ export class GetChannelsByProjectIDQuery
     this.query = gql(`
       query __GET_CHANNELS__($projectId: ID, $limit: Int${queryActiveAttr}){
         channels: project_channel_list(where: {project_id: {_equals: $projectId} ${queryActiveParams}} limit: $limit){
-      query __GET_CHANNELS__($projectId: ID){
-        channels: project_channel_list(where: {project_id: {_equals: $projectId}}, limit : 100){
           id
           is_active
           name
