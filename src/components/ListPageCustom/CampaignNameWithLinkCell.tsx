3c8f0fde6edd59387e12b0fbb1cdab56f190ef57
Dmitry Knyazkov <knyazkovd@gmail.com>
2023-10-17 16:24:06 +0300

    [SAAS-80] fix link

--- src/components/ListPageCustom/CampaignNameWithLinkCell.tsx
+++ src/components/ListPageCustom/CampaignNameWithLinkCell.tsx
@@ -1,10 +1,10 @@
-import React, { FC, MouseEventHandler, useRef } from "react";
-import { SimpleValues } from "../../services/listDataLoader/listLoader/listValues/SimpleValues";
-import { Box, TableCell } from "@mui/material";
-import { ListFieldProperties } from "../../services/listDataLoader/listLoader/types";
-import { useEntityList } from "context/EntityListContext";
-import { listSchemaConfiguration } from "settings/pages";
-import { useRouter } from "next/router";
+import React, {FC, useRef} from "react";
+import {SimpleValues} from "../../services/listDataLoader/listLoader/listValues/SimpleValues";
+import {TableCell, Link as LinkUi} from "@mui/material";
+import {ListFieldProperties} from "../../services/listDataLoader/listLoader/types";
+import {useEntityList} from "context/EntityListContext";
+import {listSchemaConfiguration} from "settings/pages";
+import Link from "next/link";
 
 /**
  * Компонент ячейки названия кампании со ссылкой на редактирование
@@ -12,46 +12,37 @@ import { useRouter } from "next/router";
  * @returns
  */
 const CampaignNameWithLinkCell: FC<ListFieldProperties<SimpleValues>> = ({
-  value,
-}) => {
-  const { data } = useEntityList();
+                                                                             value,
+                                                                         }) => {
+    const {data} = useEntityList();
 
-  const { push } = useRouter();
+    const config = useRef(listSchemaConfiguration()['campaign']);
 
-  const config = useRef(listSchemaConfiguration()['campaign']);
-
-  if (!data || !config.current) {
-    return null;
-  }
-
-  const { editPageUrl } = config.current;
-
-  const {
-    currentData: { rows },
-  } = data;
-
-  const row = rows.find((r) => r.columnValues.name.value === value.value);
-
-  const onClickHandler: MouseEventHandler<HTMLDivElement> = (event) => {
-    event.stopPropagation();
-    event.preventDefault();
-
-    if (!row) {
-      return
+    if (!data || !config.current) {
+        return null;
     }
 
-    const url = editPageUrl(row.primaryKeyValue);
+    const {editPageUrl} = config.current;
 
-    return push(url.href, url.as);
-  };
+    const {
+        currentData: {rows},
+    } = data;
 
-  return (
-    <TableCell className="list-table-cell" sx={{ textAlign: 'left' }}>
-      <Box sx={{ cursor: 'pointer', textDecoration: 'underline' }} onClick={onClickHandler}>
-        {row?.columnValues.name.value}
-      </Box>
-    </TableCell>
-  );
+    const row = rows.find((r) => r.columnValues.name.value === value.value);
+    const url = editPageUrl(row?.primaryKeyValue);
+
+    return (
+        <TableCell className="list-table-cell" sx={{textAlign: 'left'}}>
+            {
+                url ? (
+                        <Link as={url.as} href={url.href} passHref>
+                            <LinkUi>{row?.columnValues.name.value}</LinkUi>
+                        </Link>
+                    )
+                    : row?.columnValues.name.value
+            }
+        </TableCell>
+    );
 };
 
 export default CampaignNameWithLinkCell;
